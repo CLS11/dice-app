5c9ba773aa3353f12d01d7d36bef3e7d02183b1a
crystall.101600 <crystall.101600@gmail.com>
2025-02-02 05:02:45 +0000

    Navigating to dice page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:diceapp/dice_page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: DicePage(),
       ),
     );
 }

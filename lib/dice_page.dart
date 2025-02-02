38d93c94d41ce76dc461b765d5421e8c8afe9829
crystall.101600 <crystall.101600@gmail.com>
2025-02-02 16:53:50 +0000

    Added necessary functionalities to change the UI on clicking the dice.

--- lib/dice_page.dart
+++ lib/dice_page.dart
@@ -1,18 +1,59 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void onTap() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 156, 247, 10),
       appBar: AppBar(
         title: const Center(
           child: Text(
-            'DICEE',
+            'DICEE', 
+            style: TextStyle(
+              color: Colors.white,
+              ),
             ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+        child: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: GestureDetector(
+                  onTap: onTap,
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
+                ),
+              ),
+              const SizedBox(width: 50),
+              Expanded(
+                child: GestureDetector(
+                  onTap: onTap,
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
+                ),
+              ),
+            ],
           ),
-        backgroundColor: Colors.red[300],
+        ),
       ),
     );
   }
